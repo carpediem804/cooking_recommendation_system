7378ec661c6547ccf0d7790113d0a2d44edcc293
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2018-11-30 20:08:08 +0900

    backend standard

---
+++ backend/routes/doctor.js
@@ -0,0 +1,9 @@
+const { Router } = require('Express')
+const doctorModel = require('../db/models/doctor')
+const patientModel = require('../db/models/patient')
+
+const router = Router()
+
+/* ... */
+
+module.exports = router
---
+++ backend/routes/patient.js
@@ -0,0 +1,7 @@
+const { Router } = require('Express')
+const router = Router()
+const patientModel = require('../db/models/patient')
+
+/* ... */
+
+module.exports = router
