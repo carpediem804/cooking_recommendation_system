3be9f2cfea6eda2075c6417fb1d3bc4dc84165e0
Daehyun <eogussla12@gmail.com>
2018-11-30 23:44:05 +0900

    by daehyun

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Board from './views/Board.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -13,13 +15,16 @@ export default new Router({
       name: 'home',
       component: Home
     },
+      {
+          path: '/about',
+          name: 'about',
+          component: About
+      }
+    ,
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/board',
+      name: 'board',
+        component: Board
     }
   ]
 })
---
+++ src/views/Board.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="board">
+        <ul>
+            <li v-for="list in lists" :key="list.id">
+                <router-link v-bind:to="list.to">{{list.title}}</router-link>
+            </li>
+        </ul>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return{
+                lists:[],
+            }
+        },
+        created: function(){
+            this.$http.get('http://localhost:8000/post').then((res)=>{
+
+                this.lists=res.data
+
+                for(let i=0;i<this.lists.length;i++){
+
+                    this.lists[i].to="/post/"+this.lists[i].id;
+
+                }
+            })
+        },
+
+
+    }
+</script>
+
