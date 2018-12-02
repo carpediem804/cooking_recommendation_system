33026076b9bcc5e821ecce93f2bb4a4e10774c95
primayy <primayy@naver.com>
2018-12-02 22:34:08 +0900

    recommend test

---
+++ front/src/components/recommend.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="recommend">
+        <h2>요리 이름</h2>
+        {{test()}}
+    </div>
+</template>
+
+<script>
+export default {
+    name:'recommend',
+    data(){
+        return {
+            name:'',
+
+        }
+    },
+    methods:{
+        test:function(){
+            this.$http.get('http://localhost:8000/findcook/recommend').then(res=>{
+                console.log("hi")
+                console.log(res)
+                //요리이름
+                //이미지
+                //요리법
+                //url
+
+            }).catch(res=>{
+                console.log(res)
+            })
+        }
+    },
+    mounted:{
+
+    }
+}
+</script>
