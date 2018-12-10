a41a679fd945c859857aebf596e93c1ab772f3f5
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2018-12-10 23:39:05 +0900

    데이터베이스에저장가능

--- front/src/components/blogtest.vue
+++ front/src/components/blogtest.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="blogtest">
         <h1>게시글 리스트</h1>
-        <b-table striped hover :items="titleList" :fields="fields"></b-table>
+        <b-table striped hover :items="titleList" :fields="fields"  @row-clicked="myRowClickHandler">
+
+        </b-table>
         <ul v-for="c in titleList" v-bind:key="c.id" >
         <li>{{c}}<br></li>
         </ul>
@@ -15,30 +17,35 @@
         name: "blogtest",
         data :function(){
             return{
-                fields: {comment_date:{
-                    label : 'Date',
-                        sortable: true
+                fields: {
+                    // comment_date:{
+                    // label : 'Date',
+                    //     sortable: true
+                    // },
+                      blogId : {
+                          label : 'BlogNumber',
+                          sortable : true
+                     },
+                      title : {
+                       label : 'Title'
+                       },
+              // body : {
+              //       label : 'Content'
+              // },
+                      category : {
+                          label : 'Category',
+                          sortable : true,
+                     },
+                       authorname: {
+                         label : 'Author',
+                         sortable : true,
                     },
-            blogId : {
-                label : 'BlogNumber',
-                sortable : true
-            },
-              title : {
-                    label : 'Title'
-              },
-              body : {
-                    label : 'Content'
-              },
-              category : {
-                    label : 'Category',
-                  sortable : true,
-              },
-                    authorname: {
-                    label : 'Author',
-                        sortable : true,
-                    },
-                    image : {
-                    label : 'Image'
+                    //      image : {
+                    //          label : 'Image'
+                    // }
+                    hart : {
+                          lable : "like number",
+                        sortable : true
                     }
 
             },
@@ -49,6 +56,16 @@
             this.$http.get("http://localhost:8000/upload/img").then((res)=>{
                 this.titleList = res.data.bloglist;
             })
+        },
+        methods : {
+            myRowClickHandler(record, index) {
+                console.log("클릭된다된다 ");
+                
+                // 'record' will be the row data from items
+                // `index` will be the visible row number (available in the v-model 'shownItems')
+                console.log(record);
+                console.log(index);// This will be the item data for the row
+            }
         }
     }
 </script>
