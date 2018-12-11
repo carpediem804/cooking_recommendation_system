bdbae645c24936532b479f01c02746e91db781ce
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2018-12-12 03:47:15 +0900

    하트한번만

--- backend/routes/like.js
+++ backend/routes/like.js
@@ -7,7 +7,7 @@ router.post('/', function(req, res,next){
     //console.log(req);
     var upheart = req.body.heart;
     var upid = req.body.id;
-    var clickuser = req.body.useruid;
+    var clickuser = req.body.uid;
     upheart += 1;
     //console.log(r);
     console.log(upheart);
@@ -15,10 +15,15 @@ router.post('/', function(req, res,next){
     console.log(clickuser);
     likedata.findOne({_id:upid},function(err,userblog){
         console.log(userblog);
-        var likeusers = userblog.likeuserId
+        var likeusers = userblog.likeuserId;
+        console.log("likeusers : "+likeusers);
+        var updateclickuser = likeusers + clickuser;
+        console.log("updateclickuser : "+updateclickuser);
+        likeusers = String(likeusers);
+
         var n =likeusers.indexOf(clickuser);
         if(n==-1){
-            likedata.findByIdAndUpdate(upid,{$set:{heart : upheart}},{new:true},function(err,tank){
+            likedata.findByIdAndUpdate(upid,{$set:{heart : upheart,likeuserId : updateclickuser}},{new:true},function(err,tank){
                 if(err){
                     console.log(err);
                 }
@@ -27,7 +32,7 @@ router.post('/', function(req, res,next){
             })
         }
         else{
-            res.send(11111);
+            res.send("reject");
         }
     })
 
