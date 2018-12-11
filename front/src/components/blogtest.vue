665893cbf9c1352d342a6986ab4c3d226e5bc1f1
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2018-12-11 23:52:18 +0900

    관리자 추가

--- front/src/components/blogtest.vue
+++ front/src/components/blogtest.vue
@@ -6,9 +6,9 @@
             <b-col md="6" class="my-1">
                 <b-form-group horizontal label="Filter" class="mb-0">
                     <b-input-group>
-                        <b-form-input v-model="filter" placeholder="Type to Search" />
+                        <b-form-input v-model="filter" placeholder="Type to Search" class="input is is-rounded" style="width: auto"/>
                         <b-input-group-append>
-                            <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
+                            <b-btn :disabled="!filter" @click="filter = ''" class="button is-info">Clear</b-btn>
                         </b-input-group-append>
                     </b-input-group>
                 </b-form-group>
@@ -20,22 +20,49 @@
                 <!--</b-form-group>-->
             <!--</b-col>-->
         </b-row>
-        <b-table striped hover :items="titleList" :fields="fields"  @row-clicked="myRowClickHandler" @filtered="onFiltered" :filter="filter" style="margin:auto">
+        <b-table striped hover :items="titleList" :fields="fields"  @row-clicked="myRowClickHandler" @filtered="onFiltered" :filter="filter" style="margin:auto" :striped="striped">
             <template slot="action" slot-scope="row">
                 <!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
-                <b-button size="sm" @click.stop="row.toggleDetails">
+                <b-button size="sm" class="button is-info"@click.stop="row.toggleDetails">
                     {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
                 </b-button>
             </template>
             <template slot="row-details" slot-scope="row">
-                <b-card>
-                    <ul>
-                        <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
-                    </ul>
+
+                <b-card style="margin: auto" bg-variant="primary"
+                        text-variant="white"
+                        class="text-center">
+                    <b-row class="mb-2" >
+                        <b-col sm="3" class="text-center"><b>작성자:</b></b-col>
+                        <b-col>{{ row.item.authorname }}</b-col>
+                    </b-row>
+                    <b-row class="mb-2">
+                        <b-col sm="3" class="text-sm-right"><b>제목:</b></b-col>
+                        <b-col>{{ row.item.title }}</b-col>
+                    </b-row>
+                    <b-row class="mb-2">
+                        <b-col sm="3" class="text-sm-right"><b>사진:</b></b-col>
+                        <b-col><img v-bind:src=$url(test+row.item.image)></b-col>
+                    </b-row>
+                    <b-row class="mb-2">
+                        <b-col sm="3" class="text-sm-right"><b>내용:</b></b-col>
+                        <b-col>{{ row.item.body }}</b-col>
+                    </b-row>
+                    <b-button size="sm" class="button is-success" @click="row.toggleDetails">Hide Details</b-button>
                 </b-card>
+                <!--<b-card>-->
+                       <!--<ul>-->
+                        <!--<li v-for="(value, key) in row.item" :key="key">-->
+                            <!--<h1> {{key}}</h1>:<b>{{value}}</b>>-->
+                            <!--<div v-if="key==='image'">-->
+                        <!--<img v-bind:src=$url(test+value)>-->
+                            <!--</div>-->
+                        <!--</li>-->
+                    <!--</ul>-->
+                <!--</b-card>-->
             </template>
-            <template slot="remove" slot-scope="row">
-                <b-button size="lg" variant="Delete" @click="deletedata(row)">
+            <template slot="remove" slot-scope="row" >
+                <b-button size="lg" variant="Delete" class ="button is-danger" @click="deletedata(row)">
                     delete
                 </b-button>
             </template>
@@ -94,12 +121,15 @@
                // sortDesc: false,
                // sortDirection: 'asc',
                 filter: null,
-                modalInfo: { title: '', content: '' }
+                modalInfo: { title: '', content: '' },
+
+                test : 'http://localhost:8000/'
             }
         },
         created(){
             this.$http.get("http://localhost:8000/upload/img").then((res)=>{
                 this.titleList = res.data.bloglist;
+
             })
         },
         methods : {
