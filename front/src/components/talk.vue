5ce630349feea90b47c5c04200e8d0aa98af7ac5
carpediem804 <33194900+carpediem804@users.noreply.github.com>
2018-12-13 05:42:39 +0900

    qwetwetwetwet

--- front/src/components/talk.vue
+++ front/src/components/talk.vue
@@ -54,7 +54,40 @@
             }
         },
         methods:{
-
+            like:function(item){
+                console.log(this.$store.state.user)
+                this.$http.post('http://localhost:8000/like/',{
+                    heart:item.heart,
+                    id : item._id,
+                    uid:this.$store.state.user.uid
+                }).then((res)=>{
+                    if(res.data=="reject")
+                    {
+                        alert("이미 추천하셨습니다.")
+                        this.$http.get("http://localhost:8000/upload/img").then((res)=>{
+                            this.list = res.data.bloglist;
+                            console.log(this.list)
+                            for(let i=0;i<this.list.length;i++)
+                            {
+                                this.list[i].image='http://localhost:8000/'+this.list[i].image;
+                            }
+                        })
+                    }
+                    else
+                    {
+                        this.$http.get("http://localhost:8000/upload/img").then((res)=>{
+                            this.list = res.data.bloglist;
+                            console.log(this.list)
+                            for(let i=0;i<this.list.length;i++)
+                            {
+                                this.list[i].image='http://localhost:8000/'+this.list[i].image;
+                            }
+                        })
+                        alert("추천하셨습니다")
+                    }
+                })
+               // this.reList()
+            },
             reList:function () {
                 this.$http.get("http://localhost:8000/upload/img").then((res)=>{
                     this.list = res.data.bloglist;
