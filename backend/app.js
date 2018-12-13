23ddd4dcf4e511a9d1c1c249af61b53abb8b769e
Daehyun <eogussla12@gmail.com>
2018-12-13 12:58:50 +0900

    daehyun

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path');
 
 require('./db/mongo') //connect db
+
 const findcook = require('./routes/findcook')
 const upload = require('./routes/upload')
 const deletepost = require('./routes/removeblog')
 const like = require('./routes/like')
 const signup = require('./routes/signup')
 const app = express()
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+
+})
+
 
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin", "*")
